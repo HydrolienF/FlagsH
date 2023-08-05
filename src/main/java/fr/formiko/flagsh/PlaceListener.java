779d02a7a986822ebf5dffaac4e1dba198426be3
HydrolienF <hydrolien.f@gmail.com>
2023-08-05 11:55:39 +0200

    Keep the draw on the banner & keep it as close as possible.

--- src/main/java/fr/formiko/flagsh/PlaceListener.java
+++ src/main/java/fr/formiko/flagsh/PlaceListener.java
@@ -42,40 +42,46 @@ public class PlaceListener implements Listener {
         Block banner = event.getBlockPlaced();
         BlockData data = banner.getBlockData();
         Block behind = event.getBlockAgainst();
+        ItemStack itemStack = event.getItemInHand();
 
         p.sendMessage("You placed a wall banner in sneak in " + banner.getLocation() + " " + banner.getType().toString() + " "
                 + behind.getType().toString());
         p.sendMessage("blocdata: " + data.getAsString());
 
 
+        float offsetToMergeTexture = 0.02f;
         float yaw = 0;
         float offsetX = 0;
         float offsetZ = 0;
         if (behind.getX() > banner.getX()) {
             p.sendMessage("behind.getX() > banner.getX()");
             yaw = 0;
+            offsetZ = offsetToMergeTexture;
         } else if (behind.getX() < banner.getX()) {
             p.sendMessage("behind.getX() < banner.getX()");
             yaw = 180;
+            offsetZ = -offsetToMergeTexture;
         } else if (behind.getZ() > banner.getZ()) {
             p.sendMessage("behind.getZ() > banner.getZ()");
             yaw = 90;
+            offsetX = -offsetToMergeTexture;
         } else if (behind.getZ() < banner.getZ()) {
             p.sendMessage("behind.getZ() < banner.getZ()");
             yaw = -90;
+            offsetX = offsetToMergeTexture;
         }
 
 
         p.sendMessage("yaw: " + yaw + "");
 
-        Location location = new Location(banner.getWorld(), banner.getX() + offsetX + 0.5f, banner.getY() + 0.5f,
+        Location location1 = new Location(banner.getWorld(), banner.getX() + offsetX + 0.5f, banner.getY() + 0.5f,
                 banner.getZ() + offsetZ + 0.5f);
+        Location location2 = new Location(banner.getWorld(), banner.getX() - offsetX + 0.5f, banner.getY() + 0.5f,
+                banner.getZ() - offsetZ + 0.5f);
+
 
         // BlockDisplay don't work with banners
-        ItemDisplay itemDisplay = banner.getWorld().spawn(location, ItemDisplay.class);
-        // ItemStack itemStack = banner.getState().getData().toItemStack(1);
-        ItemStack itemStack = event.getItemInHand();
-        // itemStack.setItemMeta(banner.getState().getItemMeta());
+        ItemDisplay itemDisplay = banner.getWorld().spawn(location1, ItemDisplay.class);
         itemDisplay.setItemStack(itemStack);
         // @formatter:off
         itemDisplay.setTransformationMatrix(new Matrix4f(
@@ -87,12 +93,26 @@ public class PlaceListener implements Listener {
 
         itemDisplay.setRotation(yaw, 0);
 
-        // itemDisplay.remove();
+        // BlockDisplay don't work with banners
+
+        // Duplicate the texture to make it look like a flag
+        itemDisplay = banner.getWorld().spawn(location2, ItemDisplay.class);
+        itemDisplay.setItemStack(itemStack);
+        // @formatter:off
+        itemDisplay.setTransformationMatrix(new Matrix4f(
+                0, 1, 0, 0,
+                1, 0, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1));
+        // @formatter:on
+
+        itemDisplay.setRotation(yaw + 180, 0);
 
         // remove the placed banner without droping the item
         banner.setType(Material.AIR);
 
 
+        // TODO
         // Or use a custom interaction so that player can break the banner
 
         // Or make it break when block behind is broken
