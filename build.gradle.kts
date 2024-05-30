771e2d321c47dfd6ac8f093b8c828bd74f9833ff
HydrolienF <hydrolien.f@gmail.com>
2024-05-30 14:08:42 +0200

    1.20.6 & Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group="fr.formiko.flagsh"
-version="4.0.2"
+version="4.0.3"
 description="Display banners as flags."
 
 repositories {
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT") // TODO "io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT" when it will be stable
+    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
     compileOnly("com.palmergames.bukkit.towny:towny:0.100.2.0")
     implementation("org.bstats:bstats-bukkit:3.0.2")
     implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
@@ -26,7 +26,7 @@ dependencies {
 
 java {
   // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-//   toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks {
@@ -44,15 +44,7 @@ tasks {
     assemble {
         dependsOn(shadowJar)
     }
-    compileJava {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-        options.release.set(17) // See https://openjdk.java.net/jeps/247 for more information.
-    }
-    javadoc {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-    }
     processResources {
-        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
         val props = mapOf(
             "name" to project.name,
             "version" to project.version,
